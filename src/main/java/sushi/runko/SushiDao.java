cdbfa6089f0c286693f4874f53a2f73d090b0939
unknown <karvamahuli@hotmail.com>
2018-02-19 12:32:38 +0200

    Päivitys

--- src/main/java/sushi/runko/SushiDao.java
+++ src/main/java/sushi/runko/SushiDao.java
@@ -3,16 +3,16 @@ package sushi.runko;
 import java.sql.SQLException;
 import java.util.List;
 
-public class RaakaAineDao implements Dao<RaakaAine, Integer>{
-    private Database database; 
+public class SushiDao implements Dao<Sushi, Integer>{
+    private Database database;
 
     @Override
-    public RaakaAine findOne(Integer key) throws SQLException {
+    public Sushi findOne(Integer key) throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public List<RaakaAine> findAll() throws SQLException {
+    public List<Sushi> findAll() throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
