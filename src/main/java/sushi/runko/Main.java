ddf452bb9a6da749726ef0084ef66e0171cefc72
unknown <karvamahuli@hotmail.com>
2018-02-20 15:37:35 +0200

    Päivityksiä mainiin ja kokonaan uusi html -tiedosto 'ainekset.html' luotu raaka-aineiden varastoimista varten sekä pieniä päivityksiä index.html:ään

--- src/main/java/sushi/runko/Main.java
+++ src/main/java/sushi/runko/Main.java
@@ -1,29 +1,35 @@
-package sushi;
+package sushi.runko;
 
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.Spark;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
-import sushi.runko.Database;
-import sushi.runko.RaakaAine;
-import sushi.runko.RaakaAineDao;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         Database tietokanta = new Database("jdbc:sqlite:raakaaineet.db");
+
         RaakaAineDao raakaAineDao = new RaakaAineDao(tietokanta);
 
         Spark.get("/", (req, res) -> {
             HashMap map = new HashMap<>();
             map.put("/", raakaAineDao.findAll());
+//            System.out.println(raakaAineDao.findOne(1).toString());
 
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
-        
+
+        Spark.get("/ainekset", (req, res) -> {
+            HashMap map = new HashMap<>();
+            map.put("/ainekset", raakaAineDao.findAll());
+
+            return new ModelAndView(map, "ainekset");
+        }, new ThymeleafTemplateEngine());
+
         Spark.post("/", (req, res) -> {
             raakaAineDao.saveOrUpdate(new RaakaAine(null, req.queryParams("nimi")));
-            
+
             res.redirect("/");
             return "";
         });
