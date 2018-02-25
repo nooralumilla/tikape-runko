47387a6d8ef7698801277caeb051bc7a000f8b8c
Koukkari K Tuomas <tkoukkar@lx7-fuxi012.ad.helsinki.fi>
2018-02-25 17:21:09 +0200

    Merge branch 'master' of https://github.com/nooralumilla/tikape-runko
    # Conflicts:
    #	src/main/java/sushi/runko/Main.java
    #	src/main/resources/templates/ainekset.html
    #	src/main/resources/templates/sushit.html

--- src/main/java/sushi/runko/Main.java
+++ src/main/java/sushi/runko/Main.java
@@ -15,7 +15,10 @@ public class Main {
 
         RaakaAineDao raakaAineDao = new RaakaAineDao(tietokanta);
         SushiDao sushiDao = new SushiDao(tietokanta);
+<<<<<<< HEAD
         SushiRaakaAineDao sushiRaakaAineDao = new SushiRaakaAineDao(tietokanta);
+=======
+>>>>>>> b18d44d6a5ee1ff603603baed0468b2aa8bbd59d
 
         Spark.get("/", (req, res) -> {
             HashMap map = new HashMap<>();
@@ -31,20 +34,29 @@ public class Main {
 
             return new ModelAndView(map, "ainekset");
         }, new ThymeleafTemplateEngine());
+<<<<<<< HEAD
         
+=======
+
+>>>>>>> b18d44d6a5ee1ff603603baed0468b2aa8bbd59d
         Spark.get("/sushit", (req, res) -> {
             HashMap map = new HashMap<>();
             map.put("sushit", sushiDao.findAll());
 
             return new ModelAndView(map, "sushit");
         }, new ThymeleafTemplateEngine());
+<<<<<<< HEAD
         
+=======
+
+>>>>>>> b18d44d6a5ee1ff603603baed0468b2aa8bbd59d
 //        Spark.get("/sushit/:id", (req, res) -> {
 //            HashMap map = new HashMap<>();
 //            map.put("sushi", raakaAineDao.findOne(:id));
 //
 //            return new ModelAndView(map, "ainekset");
 //        }, new ThymeleafTemplateEngine());
+<<<<<<< HEAD
 
         Spark.post("/", (req, res) -> {
             raakaAineDao.saveOrUpdate(new RaakaAine(null, req.queryParams("nimi")));
@@ -82,6 +94,11 @@ public class Main {
         Spark.post("/delete/:sushi_id", (req, res) -> {
             int id = Integer.parseInt(req.params(":sushi_id"));
             sushiDao.delete(id);
+=======
+        Spark.post("/", (req, res) -> {
+            raakaAineDao.saveOrUpdate(new RaakaAine(null, req.queryParams("nimi")));
+
+>>>>>>> b18d44d6a5ee1ff603603baed0468b2aa8bbd59d
             res.redirect("/");
             return "";
         });
@@ -94,6 +111,44 @@ public class Main {
             return "";
         });
         
+
+        Spark.post("/sushit", (req, res) -> {
+            String nimi = req.queryParams("sushinNimi");
+            sushiDao.saveOrUpdate(new Sushi(null, nimi));
+
+            res.redirect("/sushit");
+            return "";
+        });
+
+        Spark.post("/ainekset", (req, res) -> {
+            String nimi = req.queryParams("raakaAineenNimi");
+            raakaAineDao.saveOrUpdate(new RaakaAine(null, nimi));
+
+            res.redirect("/ainekset");
+            return "";
+        });
+
+        Spark.post("/delete/:sushi_id", (req, res) -> {
+            int id = Integer.parseInt(req.params(":sushi_id"));
+            sushiDao.delete(id);
+            res.redirect("/");
+            return "";
+        });
+
+        Spark.post("/poista/:raakaAine_id", (req, res) -> {
+            int id = Integer.parseInt(req.params(":raakaAine_id"));
+            raakaAineDao.delete(id);
+            res.redirect("/ainekset");
+            return "";
+        });
+
+        Spark.post("/sushiRaakaAineet/:sushiRaakaAine_id", (req, res) -> {
+            int id = Integer.parseInt(req.params(":sushiRaakaAine_id"));
+            //
+            //tähän toiminnallisuutta että lisätään raaka-aine valitulle sushille
+            res.redirect("/sushit");
+            return "";
+        });
 
     }
 }
