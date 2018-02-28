f773c972dbe8f2bdb6060af1ab700df7e0ed4a89
Noora Kääriäinen <nooralumilla@Noora-MacBook-Pro.local>
2018-02-28 11:14:21 +0200

    Herokun tarjoaman tietokannan käyttöönotto?

--- src/main/java/sushi/runko/Main.java
+++ src/main/java/sushi/runko/Main.java
@@ -1,5 +1,7 @@
 package sushi.runko;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -154,5 +156,16 @@ public class Main {
             res.redirect("/sushit/" + sushiId);
             return "";
         });
+
     }
+
+    public static Connection getConnection() throws Exception {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        if (dbUrl != null && dbUrl.length() > 0) {
+            return DriverManager.getConnection(dbUrl);
+        }
+
+        return DriverManager.getConnection("jdbc:sqlite:raakaaineet.db");
+    }
+
 }
